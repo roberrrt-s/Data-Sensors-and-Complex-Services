f4cdd06be51b055e50b96b9d0b7e23e404cb07f7
Art <artzeeman97@gmail.com>
2020-03-19 18:32:33 +0100

    serviceworker now uses fancy notification options

--- dscs-front-end/public/custom-sw.js
+++ dscs-front-end/public/custom-sw.js
@@ -2,7 +2,27 @@ self.addEventListener('push', event => {
   const data = event.data.json()
   console.log('New notification', data)
   const options = {
+    // Visual Options
     body: data.body,
+    icon: data.icon,
+    image: data.image,
+    badge: data.badge,
+    vibrate: data.vibrate,
+    sound: data.sound,
+    dir: data.dir,
+
+    // Behavioural Options
+    tag: data.tag,
+    data: data.data,
+    requireInteraction: data.requireInteraction,
+    renotify: data.renotify,
+    silent: data.silent,
+
+    // Both Visual & Behavioural Options
+    actions: data.actions,
+
+    // Information Option. No visual affect.
+    timestamp: data.timestamp
   }
   event.waitUntil(
     self.registration.showNotification(data.title, options)
